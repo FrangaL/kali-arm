a3537776c27f4eb55a63486490e186fc033ddd52
Steev Klimaszewski <steev@kali.org>
2020-04-07 13:56:59 -0500

    rpi3-64: Don't clean apt after otherwise it removes the downloads

--- rpi3-64.sh
+++ rpi3-64.sh
@@ -251,8 +251,6 @@ rm -f /usr/sbin/policy-rc.d
 rm -f /usr/sbin/invoke-rc.d
 dpkg-divert --remove --rename /usr/sbin/invoke-rc.d
 rm -rf /root/.bash_history
-apt-get update
-apt-get clean
 rm -f /0
 rm -f /hs_err*
 rm -f cleanup
