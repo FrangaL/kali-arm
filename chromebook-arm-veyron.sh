d471dc3b0ee9f0c2dce787631084688044543f7a
Steev Klimaszewski <steev@kali.org>
2020-07-07 16:19:58 -0500

    veyron: Back to mainline...

--- chromebook-arm-veyron.sh
+++ chromebook-arm-veyron.sh
@@ -229,10 +229,13 @@ cd "${basedir}"
 
 # Kernel section.  If you want to use a custom kernel, or configuration, replace
 # them in this section.
-#git clone --depth 1 https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y "${basedir}"/kali-${architecture}/usr/src/kernel
-git clone --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel.git -b chromeos-4.19 "${basedir}"/kali-${architecture}/usr/src/kernel
+git clone --depth 1 https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y "${basedir}"/kali-${architecture}/usr/src/kernel
+#git clone --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel.git -b chromeos-4.19 "${basedir}"/kali-${architecture}/usr/src/kernel
 cd "${basedir}"/kali-${architecture}/usr/src/kernel
-cp "${basedir}"/../kernel-configs/veyron-4.19-cros.config .config
+# Mainline kernel config
+cp "${basedir}"/../kernel-configs/veyron-4.19.config .config
+# (Currently not working) chromeos-based kernel config
+#cp "${basedir}"/../kernel-configs/veyron-4.19-cros.config .config
 cp .config "${basedir}"/kali-${architecture}/usr/src/veyron.config
 export ARCH=arm
 # Edit the CROSS_COMPILE variable as needed.
