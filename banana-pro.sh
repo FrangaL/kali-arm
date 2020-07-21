2d5521c2f64da6f1f180c49f9173e0b6e37393a3
Steev Klimaszewski <steev@kali.org>
2020-07-21 15:20:55 -0500

    banana-pro: Fix image filename

--- banana-pro.sh
+++ banana-pro.sh
@@ -16,7 +16,7 @@ machine=$(tr -cd 'A-Za-z0-9' < /dev/urandom | head -c16 ; echo)
 # Custom hostname variable
 hostname=${2:-kali}
 # Custom image file name variable - MUST NOT include .img at the end.
-imagename=${3:-kali-linux-$1-rpi3-nexmon-64-lite}
+imagename=${3:-kali-linux-$1-bananapro}
 # Suite to use, valid options are:
 # kali-rolling, kali-dev, kali-bleeding-edge, kali-dev-only, kali-experimental, kali-last-snapshot
 suite=${suite:-"kali-rolling"}
