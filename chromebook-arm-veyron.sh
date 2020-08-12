c97011182d6f21ad0d367c2911164061ab781580
Steev Klimaszewski <steev@kali.org>
2020-08-12 09:26:52 -0500

    veyron: Full clone kernel
    We can't go back to an old commit if we --depth 1 :(

--- chromebook-arm-veyron.sh
+++ chromebook-arm-veyron.sh
@@ -317,12 +317,12 @@ cd ${basedir}
 # Kernel section.  If you want to use a custom kernel, or configuration, replace
 # them in this section.
 # Mainline kernel branch
-git clone --depth 1 https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y ${work_dir}/usr/src/kernel
+git clone https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y ${work_dir}/usr/src/kernel
 # ChromeOS kernel branch
 #git clone --depth 1 https://chromium.googlesource.com/chromiumos/third_party/kernel.git -b release-${kernel_release} ${work_dir}/usr/src/kernel
 cd ${work_dir}/usr/src/kernel
 # Check out 4.19.133 which was known to work...
-git checkout  17a87580a8856170d59aab302226811a4ae69149
+git checkout 17a87580a8856170d59aab302226811a4ae69149
 # Mainline kernel config
 cp ${basedir}/../kernel-configs/veyron-4.19.config .config
 # (Currently not working) chromeos-based kernel config
