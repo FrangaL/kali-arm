dde7909de2392de3e0276c61767dc86a2820db43
g0t mi1k <have.you.g0tmi1k@gmail.com>
2021-05-06 22:29:19 +0100

    rpi-64bit* - consistency with other build scripts

--- rpi-64bit-minimal.sh
+++ rpi-64bit-minimal.sh
@@ -1,6 +1,12 @@
-#!/bin/bash -e
-# This is the Raspberry Pi 2 v1.2/3/4 Kali ARM 64 bit build script - http://www.kali.org/downloads
-# A trusted Kali Linux image created by Offensive Security - http://www.offensive-security.com
+#!/bin/bash
+set -e
+
+# This is Kali Linux ARM image for Raspberry Pi 2 v1.2/3/4/400 (64-bit) (Minimal)
+# More information:
+# - https://www.kali.org/docs/arm/raspberry-pi-2-1.2/
+# - https://www.kali.org/docs/arm/raspberry-pi-3/
+# - https://www.kali.org/docs/arm/raspberry-pi-4/
+# - https://www.kali.org/docs/arm/raspberry-pi-400/
 
 # shellcheck disable=SC2154
 # Load general functions
@@ -12,57 +18,70 @@ hw_model=${hw_model:-"rpi4"}
 # Architecture
 architecture=${architecture:-"arm64"}
 # Variant name for image and dir build
-variant=${variant:-"nexmon-${architecture}-lite"}
+variant=${variant:-"${architecture}-minimal"}
 # Desktop manager (xfce, gnome, i3, kde, lxde, mate, e17 or none)
 desktop=${desktop:-"none"}
 
 # Load common variables
 include variables
-# Checks script enviroment
+
+# Checks script environment
 include check
+
 # Packages build list
 include packages
+
 # Load automatic proxy configuration
 include proxy_apt
+
 # Execute initial debootstrap
 debootstrap_exec http://http.kali.org/kali
+
 # Enable eatmydata in compilation
 include eatmydata
+
 # debootstrap second stage
 systemd-nspawn_exec eatmydata /debootstrap/debootstrap --second-stage
+
 # Define sources.list
 include sources.list
+
 # APT options
 include apt_options
+
 # So X doesn't complain, we add kali to hosts
 include hosts
+
 # Set hostname
 set_hostname "${hostname}"
+
 # Network configs
 include network
 add_interface eth0
-# Copy directory bsp into build dir.
+
+# Copy directory bsp into build dir
 cp -rp bsp "${work_dir}"
 
 # Third stage
-cat <<EOF >"${work_dir}"/third-stage
+cat << EOF > "${work_dir}"/third-stage
 #!/bin/bash -e
 
 export DEBIAN_FRONTEND=noninteractive
+
 eatmydata apt-get update
 eatmydata apt-get -y install ${third_stage_pkgs}
 
-eatmydata apt-get install -y ${packages} || eatmydata apt-get install -y --fix-broken
-eatmydata apt-get install -y ${desktop_pkgs} ${extra} || eatmydata apt-get install -y --fix-broken
+eatmydata apt-get -y install ${packages} || eatmydata apt-get -y install --fix-broken
+eatmydata apt-get -y install ${desktop_pkgs} ${extra} || eatmydata apt-get -y install --fix-broken
 
 eatmydata apt-get -y --purge autoremove
 
-# Linux console/Keyboard configuration
+# Linux console/keyboard configuration
 echo 'console-common console-data/keymap/policy select Select keymap from full list' | debconf-set-selections
 echo 'console-common console-data/keymap/full select en-latin1-nodeadkeys' | debconf-set-selections
 
 # Copy all services
-cp -p /bsp/services/all/*.service /etc/systemd/system/
+install -m644 /bsp/services/all/*.service /etc/systemd/system/
 cp -p /bsp/services/rpi/*.service /etc/systemd/system/
 
 # Re4son's rpi-tft configurator
@@ -77,10 +96,10 @@ install -m755 /bsp/scripts/monstop /usr/bin/
 echo "deb http://http.re4son-kernel.com/re4son kali-pi main" > /etc/apt/sources.list.d/re4son.list
 wget -qO /etc/apt/trusted.gpg.d/kali_pi-archive-keyring.gpg https://re4son-kernel.com/keys/http/kali_pi-archive-keyring.gpg
 eatmydata apt-get update
-eatmydata apt-get install -y kalipi-kernel kalipi-bootloader kalipi-re4son-firmware kalipi-kernel-headers
+eatmydata apt-get -y install kalipi-kernel kalipi-bootloader kalipi-re4son-firmware kalipi-kernel-headers
 
-# Regenerated the shared-mime-info database on the first boot
-# since it fails to do so properly in a chroot.
+# Regenerate the shared-mime-info database on the first boot
+# since it fails to do so properly in a chroot
 systemctl enable smi-hack
 
 # Copy script rpi-resizerootfs
@@ -93,7 +112,7 @@ systemctl enable rpi-resizerootfs
 systemctl enable regenerate_ssh_host_keys
 
 # Copy in the bluetooth firmware
-install -m644 /bsp/firmware/rpi/BCM43430A1.hcd /lib/firmware/brcm/
+install -m644 /bsp/firmware/rpi/BCM43430A1.hcd -D /lib/firmware/brcm/BCM43430A1.hcd
 # Copy rule and service
 install -m644 /bsp/bluetooth/rpi/99-com.rules /etc/udev/rules.d/
 install -m644 /bsp/bluetooth/rpi/hciuart.service /etc/systemd/system/
@@ -105,7 +124,7 @@ systemctl enable hciuart
 # Enable copying of user wpa_supplicant.conf file
 systemctl enable copy-user-wpasupplicant
 
-# Enable... enabling ssh by putting ssh or ssh.txt file in /boot
+# Enable sshd by putting ssh or ssh.txt file in /boot
 systemctl enable enable-ssh
 
 # Allow users to use NM over ssh
@@ -117,16 +136,16 @@ apt download -o APT::Sandbox::User=root ca-certificates 2>/dev/null
 # Copy over the default bashrc
 cp /etc/skel/.bashrc /root/.bashrc
 
-# Set a REGDOMAIN.  This needs to be done or wireless doesn't work correctly on the RPi 3B+
+# Set a REGDOMAIN. This needs to be done or wireless may not work correctly on the RPi 3B+
 sed -i -e 's/REGDOM.*/REGDOMAIN=00/g' /etc/default/crda
 
 # Enable login over serial
 echo "T0:23:respawn:/sbin/agetty -L ttyAMA0 115200 vt100" >> /etc/inittab
 
 # Try and make the console a bit nicer
-# Set the terminus font for a bit nicer display.
-sed -i -e 's/FONTFACE=.*/FONTFACE="Terminus"/' /etc/default/console-setup
-sed -i -e 's/FONTSIZE=.*/FONTSIZE="6x12"/' /etc/default/console-setup
+# Set the terminus font for a bit nicer display
+sed -i -e 's/FONTFACE=.*/FONTFACE="Terminus"/g' /etc/default/console-setup
+sed -i -e 's/FONTSIZE=.*/FONTSIZE="6x12"/g' /etc/default/console-setup
 
 # Fix startup time from 5 minutes to 15 secs on raise interface wlan0
 sed -i 's/^TimeoutStartSec=5min/TimeoutStartSec=15/g' "/usr/lib/systemd/system/networking.service"
@@ -147,30 +166,37 @@ systemd-nspawn_exec /third-stage
 
 # Configure Raspberry PI firmware
 include rpi_firmware
+
 # Compile Raspberry PI userland
 include rpi_userland
+
 # Choose a locale
 set_locale "$locale"
+
 # Clean system
 include clean_system
-# Define DNS server after last running systemd-nspawn.
-echo "nameserver 8.8.8.8" >"${work_dir}"/etc/resolv.conf
-# Disable the use of http proxy in case it is enabled.
+
+# Define DNS server after last running systemd-nspawn
+echo "nameserver 8.8.8.8" > "${work_dir}"/etc/resolv.conf
+
+# Disable the use of http proxy in case it is enabled
 disable_proxy
+
 # Mirror & suite replacement
 restore_mirror
+
 # Reload sources.list
 include sources.list
 
-# systemd doesn't seem to be generating the fstab properly for some people, so let's create one.
-cat <<EOF >"${work_dir}"/etc/fstab
+# systemd doesn't seem to be generating the fstab properly for some people, so let's create one
+cat << EOF > "${work_dir}"/etc/fstab
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
 proc            /proc           proc    defaults          0       0
 /dev/mmcblk0p1  /boot           vfat    defaults          0       2
 /dev/mmcblk0p2  /               $fstype    defaults,noatime  0       1
 EOF
 
-# Calculate the space to create the image and create.
+# Calculate the space to create the image and create
 make_image
 
 # Create the disk partitions it
@@ -201,10 +227,12 @@ mount "${bootp}" "${basedir}"/root/boot
 
 log "Rsyncing rootfs into image file" green
 rsync -HPavz -q --exclude boot "${work_dir}"/ "${basedir}"/root/
-rsync -rtx -q "${work_dir}"/boot "${basedir}"/root
-sync
-
-# Umount filesystem
+rsync -rtx -q "${work_dir}"/boot "${basedir}"/root/
+# Start to unmount partition(s)
+sync; sync
+# sleep for 10 seconds, to let the cache settle after sync
+sleep 10
+# Unmount filesystem
 umount -l "${bootp}"
 umount -l "${rootp}"
 
@@ -212,12 +240,12 @@ umount -l "${rootp}"
 dosfsck -w -r -a -t "$bootp"
 e2fsck -y -f "$rootp"
 
-# Remove loop devices
+# Remove loop device
 losetup -d "${loopdevice}"
 
 # Compress image compilation
 include compress_img
 
-# Clean up all the temporary build stuff and remove the directories.
-# Comment this out to keep things around if you want to see what may have gone wrong.
+# Clean up all the temporary build stuff and remove the directories
+# Comment this out to keep things around if you want to see what may have gone wrong
 clean_build
