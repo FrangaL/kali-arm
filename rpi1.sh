13f02f6c2cb0660b9162104a72196c64302a3b01
Steev Klimaszewski <steev@kali.org>
2021-09-10 09:55:11 -0500

    rpi1: Install systemd-timesyncd

--- rpi1.sh
+++ rpi1.sh
@@ -54,6 +54,8 @@ eatmydata apt-get -y install ${third_stage_pkgs}
 
 eatmydata apt-get install -y ${packages} || eatmydata apt-get install -y --fix-broken
 eatmydata apt-get install -y ${desktop_pkgs} ${extra} || eatmydata apt-get install -y --fix-broken
+# ntp doesn't always sync the date, but systemd's timesyncd does, so we remove ntp and reinstall it with this.
+eatmydata apt-get install -y systemd-timesyncd --autoremove
 
 eatmydata apt-get -y --purge autoremove
 
