0866498fea79ccf13474d30b42aa6e41e6b532cf
Steev Klimaszewski <steev@kali.org>
2021-09-16 14:01:29 -0500

    rpi3: Attempt to flush the buffers and bytes
    Lets see if adding the flushing of buffers and bytes assists in getting the build server to not have issues with cleanups.

--- rpi3-nexmon.sh
+++ rpi3-nexmon.sh
@@ -187,6 +187,10 @@ rsync -HPavz -q --exclude boot "${work_dir}"/ "${basedir}"/root/
 rsync -rtx -q "${work_dir}"/boot "${basedir}"/root
 sync
 
+# Flush buffers and bytes - this is nicked from the Devuan arm-sdk.
+blockdev --flushbufs "${loopdevice}"
+python -c 'import os; os.fsync(open("'${loopdevice}'", "r+b"))'
+
 # Umount filesystem
 umount -l "${bootp}"
 umount -l "${rootp}"
