690eea2accb6c458a71361049ff269278cd0a95c
Ben Wilson <g0tmi1k@kali.org>
2021-09-17 10:49:11 +0100

    Restructure build-deps.sh

--- common.d/build-deps.sh
+++ common.d/build-deps.sh
@@ -7,31 +7,34 @@
 # Stop on error
 set -e
 
-# Check permissions script
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root" >&2
-  exit 1
-fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Check compatible systems
-if ! which dpkg > /dev/null; then
-   echo "Script only compatible with Debian-based systems" >&2
-   exit 1
-fi
+apt-wait() {
+  while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    sleep 5
+  done
 
-# List of installed packages file
-backup_packages=list-debs-$(date +"%H_%M_%m_%d_%Y")
-# Create a current list of installed packages
-dpkg --get-selections > ${backup_packages}
+  if [ "$1" == "update" ]; then
+    apt-get update
+  elif [ "$1" == "install_deps" ]; then
+    echo "Installing $deps..."
+    apt-get install -y -qq $deps
+  elif [ "$1" == "remove" ]; then
+    echo "Removing $@..."
+    apt-get -y --purge "$@"
+  fi
+}
+
 
 # Function create script to clean system packages
-clean-system () {
-cat << EOF > clean_system${backup_packages//list-debs/}.sh
+clean-system() {
+  echo "Cleaning up..."
+  cat << EOF > clean_system${backup_packages//list-debs/}.sh
 #!/bin/bash -e
 
 clean_system () {
   dpkg --clear-selections
-  dpkg --set-selections < ${backup_packages}
+  dpkg --set-selections < $(cat ${backup_packages})
   apt-get -y dselect-upgrade
   apt-get -y remove --purge \$(dpkg -l | grep "^rc" | awk '{print \$2}')
   ${del_arch_i386}
@@ -46,9 +49,32 @@ case \$yn in
       * ) echo "Please enter Y or N!";;
 esac
 EOF
-chmod 755 clean_system${backup_packages//list-debs/}.sh
+  chmod 0755 clean_system${backup_packages//list-debs/}.sh
 }
+
 trap clean-system ERR SIGTERM SIGINT
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Check permissions script
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root" >&2
+  exit 1
+fi
+
+# Check compatible systems
+if ! which dpkg > /dev/null; then
+   echo "Script only compatible with Debian-based systems" >&2
+   exit 1
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# List of installed packages file
+backup_packages=$(mktemp)
+
+# Create a current list of installed packages
+dpkg --get-selections > ${backup_packages}
 
 compilers="crossbuild-essential-arm64 crossbuild-essential-armhf crossbuild-essential-armel gcc-arm-none-eabi"
 libpython2_dev="libexpat1-dev libpython2.7 libpython2.7-dev libpython2.7-minimal libpython2.7-stdlib"
@@ -60,36 +86,36 @@ ${libpython2_dev} dbus python3-dev cgroup-tools lsof jetring eatmydata cmake pkg
 
 deps="${dependencies} ${compilers}"
 
-apt-wait () {
-  while fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
-    sleep 5
-  done
-
-  if [ "$1" == "update" ];then
-    apt-get update
-  elif [ "$1" == "install_deps" ];then
-    apt-get install -y -qq $deps
-  elif [ "$1" == "remove" ];then
-    apt-get -y --purge "$@"
-  fi
-}
-
 # Update list deb packages
 apt-wait update
 # Install dependencies
 apt-wait install_deps
 
+# Check minimum version debootstrap
+debootstrap_ver=$(debootstrap --version | grep -o '[0-9.]\+' | head -1)
+if dpkg --compare-versions "$debootstrap_ver" lt "1.0.105"; then
+  echo "Currently your version of debootstrap ($debootstrap_ver) does not support the script" >&2
+  echo "The minimum version of debootstrap is 1.0.105" >&2
+  exit 1
+fi
+
 # Install kali-archive-keyring
 if [ ! -f /usr/share/keyrings/kali-archive-keyring.gpg ]; then
+  echo "Installing kali-archive-keyring..."
   temp_key="$(mktemp -d)"
   git clone https://gitlab.com/kalilinux/packages/kali-archive-keyring.git $temp_key
-  cd $temp_key && make && make install && cd $OLDPWD && rm -rf $temp_key
+  cd $temp_key/
+  make
+  make install
+  cd $OLDPWD/
+  rm -rf $temp_key
 fi
 
 echo "Waiting for other software manager to finish..."
 
 # Install packages i386
 if [ $(arch) == 'x86_64' ]; then
+  echo "Detected x64"
   if [ -z $(dpkg --print-foreign-architectures | grep i386) ]; then
     dpkg --add-architecture i386
     apt-wait update
@@ -107,11 +133,3 @@ fi
 
 # Create the script to clean the system
 clean-system
-
-# Check minimum version debootstrap
-debootstrap_ver=$(debootstrap --version | grep -o '[0-9.]\+' | head -1)
-if dpkg --compare-versions "$debootstrap_ver" lt "1.0.105"; then
-    echo "Currently your version of debootstrap does not support the script." >&2
-    echo "The minimum version of debootstrap is 1.0.105" >&2
-    exit 1
-fi
