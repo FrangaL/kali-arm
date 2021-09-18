053596fbff69fe18b53fbc9bc079323e8508903b
Ben Wilson <g0tmi1k@kali.org>
2021-09-18 12:11:18 +0100

    More verbose with image output

--- common.d/compress_img.sh
+++ common.d/compress_img.sh
@@ -5,11 +5,13 @@ log "compress image" green
 if [ "${compress:=}" = xz ]; then
   log "Compressing file: $(tput sgr0) ${imagename:=}.img" green
   if [ "$(arch)" == 'x86_64' ] || [ "$(arch)" == 'aarch64' ]; then
-    limit_cpu pixz -p "${num_cores:=}" "${image_dir:=}"/"${imagename}".img # -p Nº cpu cores use
+    limit_cpu pixz -p "${num_cores:=}" "${image_dir:=}/${imagename:=}.img" # -p Nº cpu cores use
   else
-    xz --memlimit-compress=50% -T "$num_cores" "${image_dir}"/"${imagename}".img # -T Nº cpu cores use
+    xz --memlimit-compress=50% -T "$num_cores" "${image_dir:=}/${imagename:=}.img" # -T Nº cpu cores use
   fi
-  chmod 0644 "${image_dir}"/"${imagename}".img.xz
+  chmod 0644 "${image_dir:=}/${imagename:=}.img.xz"
+  stat "${image_dir:=}/${imagename:=}.img.xz"
 else
-  chmod 0644 "${image_dir}"/"${imagename}".img
+  chmod 0644 "${image_dir:=}/${imagename:=}.img"
+  stat "${image_dir:=}/${imagename:=}.img"
 fi
