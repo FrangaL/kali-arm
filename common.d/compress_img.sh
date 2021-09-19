cb8d1dfb5a504c1375f06ee4ffd12e8b0089b8f2
Ben Wilson <g0tmi1k@kali.org>
2021-09-19 19:50:19 +0100

    Improve logic for compression

--- common.d/compress_img.sh
+++ common.d/compress_img.sh
@@ -3,15 +3,13 @@
 log "compress image" green
 
 if [ "${compress:=}" = xz ]; then
-  log "Compressing file: $(tput sgr0) ${image_name:=}.img" green
+  log "Compressing file: $(tput sgr0) ${image_dir}/${image_name}.img" green
   if [ "$(arch)" == 'x86_64' ] || [ "$(arch)" == 'aarch64' ]; then
-    limit_cpu pixz -p "${num_cores:=}" "${image_dir:=}/${image_name:=}.img" # -p Nº cpu cores use
+    limit_cpu pixz -p "${num_cores:=}" "${image_dir}/${image_name}.img" # -p Nº cpu cores use
   else
-    xz --memlimit-compress=50% -T "$num_cores" "${image_dir:=}/${image_name:=}.img" # -T Nº cpu cores use
+    xz --memlimit-compress=50% -T "$num_cores" "${image_dir}/${image_name}.img" # -T Nº cpu cores use
   fi
-  chmod 0644 "${image_dir:=}/${image_name:=}.img.xz"
-  stat "${image_dir:=}/${image_name:=}.img.xz"
-else
-  chmod 0644 "${image_dir:=}/${image_name:=}.img"
-  stat "${image_dir:=}/${image_name:=}.img"
 fi
+
+chmod 0644 "${image_dir}/${image_name}.img"*
+stat "${image_dir}/${image_name}.img"*
