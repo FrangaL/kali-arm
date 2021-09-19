65c7481378c30a58f7f0c0ad09921e7ac1289b0e
neuronaddict <manufrommars@elacourarie.org>
2021-09-20 01:18:48 +0200

    nanopi2.sh: convert script to the new way.

--- nanopi2.sh
+++ nanopi2.sh
@@ -13,201 +13,56 @@ set -e
 # Uncomment to activate debug
 # debug=true
 
-if [ "$debug" = true ]; then
-  exec > >(tee -a -i "${0%.*}.log") 2>&1
-  set -x
-fi
+# shellcheck disable=SC2154
+# Load general functions
+# shellcheck source=/dev/null
+source ./common.d/functions.sh
 
+
+
+# Hardware model
+hw_model=${hw_model:-"nanopi2"}
 # Architecture
 architecture=${architecture:-"armhf"}
-# Generate a random machine name to be used
-machine=$(tr -cd 'A-Za-z0-9' < /dev/urandom | head -c16 ; echo)
-# Custom hostname variable
-hostname=${2:-kali}
-# Custom image file name variable - MUST NOT include .img at the end
-image_name=${3:-kali-linux-$1-nanopi2}
-# Suite to use, valid options are:
-# kali-rolling, kali-dev, kali-bleeding-edge, kali-dev-only, kali-experimental, kali-last-snapshot
-suite=${suite:-"kali-rolling"}
-# Free space rootfs in MiB
-free_space="300"
-# /boot partition in MiB
-bootsize="128"
-# Select compression, xz or none
-compress="xz"
-# Choose filesystem format to format (ext3 or ext4)
-fstype="ext3"
-# If you have your own preferred mirrors, set them here
-mirror=${mirror:-"http://http.kali.org/kali"}
-# GitLab URL Kali repository
-kaligit="https://gitlab.com/kalilinux"
-# GitHub raw URL
-githubraw="https://raw.githubusercontent.com"
+# Variant name for image and dir build
+variant=${variant:-"${architecture}"}
+# Desktop manager (xfce, gnome, i3, kde, lxde, mate, e17 or none)
+desktop=${desktop:-"xfce"}
 
-# Check EUID=0 you can run any binary as root
-if [[ $EUID -ne 0 ]]; then
-  echo "This script must be run as root or have super user permissions" >&2
-  echo "Use: sudo $0 ${1:-2.0} ${2:-kali}" >&2
-  exit 1
-fi
-
-# Pass version number
-if [[ $# -eq 0 ]] ; then
-  echo "Please pass version number, e.g. $0 2.0, and (if you want) a hostname, default is kali" >&2
-  exit 0
-fi
-
-# Check exist bsp directory
-if [ ! -e "bsp" ]; then
-  echo "Error: missing bsp directory structure" >&2
-  echo "Please clone the full repository ${kaligit}/build-scripts/kali-arm" >&2
-  exit 255
-fi
-
-# Current directory
-current_dir="$(pwd)"
-# Base directory
-base_dir=${current_dir}/nanopi2-"$1"
-# Working directory
-work_dir="${base_dir}/kali-${architecture}"
-
-# Check directory build
-if [ -e "${base_dir}" ]; then
-  echo "${base_dir} directory exists, will not continue" >&2
-  exit 1
-elif [[ ${current_dir} =~ [[:space:]] ]]; then
-  echo "The directory "\"${current_dir}"\" contains whitespace. Not supported." >&2
-  exit 1
-else
-  echo "The base_dir thinks it is: ${base_dir}"
-  mkdir -p ${base_dir}
-fi
-
-components="main,contrib,non-free"
-arm="kali-linux-arm ntpdate"
-base="apt-transport-https apt-utils bash-completion console-setup dialog e2fsprogs ifupdown initramfs-tools inxi iw man-db mlocate netcat-traditional net-tools parted psmisc rfkill screen tmux unrar usbutils vim wget whiptail zerofree"
-desktop="kali-desktop-xfce kali-root-login xserver-xorg-video-fbdev xfonts-terminus xinput"
-tools="kali-linux-default"
-services="apache2 atftpd"
-extras="alsa-utils bc bison bluez bluez-firmware i2c-tools kali-linux-core libnss-systemd libssl-dev python3-smbus triggerhappy"
-
-packages="${arm} ${base} ${services}"
-
-# Automatic configuration to use an http proxy, such as apt-cacher-ng
-# You can turn off automatic settings by uncommenting apt_cacher=off
-# apt_cacher=off
-# By default the proxy settings are local, but you can define an external proxy
-# proxy_url="http://external.intranet.local"
-apt_cacher=${apt_cacher:-"$(lsof -i :3142|cut -d ' ' -f3 | uniq | sed '/^\s*$/d')"}
-if [ -n "$proxy_url" ]; then
-  export http_proxy=$proxy_url
-elif [ "$apt_cacher" = "apt-cacher-ng" ] ; then
-  if [ -z "$proxy_url" ]; then
-    proxy_url=${proxy_url:-"http://127.0.0.1:3142/"}
-    export http_proxy=$proxy_url
-  fi
-fi
-
-# Detect architecture
-if [[ "${architecture}" == "arm64" ]]; then
-        qemu_bin="/usr/bin/qemu-aarch64-static"
-        lib_arch="aarch64-linux-gnu"
-elif [[ "${architecture}" == "armhf" ]]; then
-        qemu_bin="/usr/bin/qemu-arm-static"
-        lib_arch="arm-linux-gnueabihf"
-elif [[ "${architecture}" == "armel" ]]; then
-        qemu_bin="/usr/bin/qemu-arm-static"
-        lib_arch="arm-linux-gnueabi"
-fi
-
-# create the rootfs - not much to modify here, except maybe throw in some more packages if you want
-eatmydata debootstrap --foreign --keyring=/usr/share/keyrings/kali-archive-keyring.gpg --include=kali-archive-keyring,eatmydata \
-  --components=${components} --arch ${architecture} ${suite} ${work_dir} http://http.kali.org/kali
-
-# systemd-nspawn environment
-systemd-nspawn_exec(){
-  LANG=C systemd-nspawn -q --bind-ro ${qemu_bin} -M ${machine} -D ${work_dir} "$@"
-}
-
-# We need to manually extract eatmydata to use it for the second stage
-for archive in ${work_dir}/var/cache/apt/archives/*eatmydata*.deb; do
-  dpkg-deb --fsys-tarfile "$archive" > ${work_dir}/eatmydata
-  tar -xkf ${work_dir}/eatmydata -C ${work_dir}
-  rm -f ${work_dir}/eatmydata
-done
-
-# Prepare dpkg to use eatmydata
-systemd-nspawn_exec dpkg-divert --divert /usr/bin/dpkg-eatmydata --rename --add /usr/bin/dpkg
-
-cat > ${work_dir}/usr/bin/dpkg << EOF
-#!/bin/sh
-if [ -e /usr/lib/${lib_arch}/libeatmydata.so ]; then
-    [ -n "\${LD_PRELOAD}" ] && LD_PRELOAD="\$LD_PRELOAD:"
-    LD_PRELOAD="\$LD_PRELOAD\$so"
-fi
-for so in /usr/lib/${lib_arch}/libeatmydata.so; do
-    [ -n "\$LD_PRELOAD" ] && LD_PRELOAD="\$LD_PRELOAD:"
-    LD_PRELOAD="\$LD_PRELOAD\$so"
-done
-export LD_PRELOAD
-exec "\$0-eatmydata" --force-unsafe-io "\$@"
-EOF
-chmod 0755 ${work_dir}/usr/bin/dpkg
-
+# Load common variables
+include variables
+# Checks script environment
+include check
+# Packages build list
+include packages
+# Execute initial debootstrap
+debootstrap_exec http://http.kali.org/kali
+# Enable eatmydata in compilation
+include eatmydata
 # debootstrap second stage
 systemd-nspawn_exec eatmydata /debootstrap/debootstrap --second-stage
-
-cat << EOF > ${work_dir}/etc/apt/sources.list
-deb ${mirror} ${suite} ${components//,/ }
-#deb-src ${mirror} ${suite} ${components//,/ }
-EOF
-
-mkdir -p ${work_dir}/etc/network/
-cat << EOF > ${work_dir}/etc/network/interfaces
-auto lo
-iface lo inet loopback
-
-auto eth0
-allow-hotplug eth0
-iface eth0 inet dhcp
-
-# This prevents NetworkManager from attempting to use this
-# device to connect to wifi, since NM doesn't show which device is which
-# Unfortunately, it still SHOWS the device, just that it's not managed
-iface p2p0 inet manual
-EOF
-
-# Set hostname
-echo "${hostname}" > ${work_dir}/etc/hostname
-
+# Define sources.list
+include sources.list
+# APT options
+include apt_options
 # So X doesn't complain, we add kali to hosts
-cat << EOF > ${work_dir}/etc/hosts
-127.0.0.1       ${hostname}    localhost
-::1             localhost ip6-localhost ip6-loopback
-fe00::0         ip6-localnet
-ff00::0         ip6-mcastprefix
-ff02::1         ip6-allnodes
-ff02::2         ip6-allrouters
-EOF
-
-# Disable IPv6
-cat << EOF > ${work_dir}/etc/modprobe.d/ipv6.conf
-# Don't load ipv6 by default
-alias net-pf-10 off
-EOF
-
-# DNS server
-echo "nameserver ${nameserver}" > "${work_dir}"/etc/resolv.conf
+include hosts
+# Set hostname
+set_hostname "${hostname}"
+# Network configs
+include network
+add_interface eth0
 
 # Copy directory bsp into build dir
-cp -rp bsp ${work_dir}
+status "Copy directory bsp into build dir"
+cp -rp bsp "${work_dir}"
 
-export MALLOC_CHECK_=0 # workaround for LP: #520465
+# Disable RESUME (suspend/resume is currently broken anyway!) which speeds up boot massively
+mkdir -p ${work_dir}/etc/initramfs-tools/conf.d/
+cat << EOF > ${work_dir}/etc/initramfs-tools/conf.d/resume
+RESUME=none
+EOF
 
-# Enable the use of http proxy in third-stage in case it is enabled
-if [ -n "$proxy_url" ]; then
-  echo "Acquire::http { Proxy \"$proxy_url\" };" > ${work_dir}/etc/apt/apt.conf.d/66proxy
-fi
 
 # Third stage
 cat << EOF >  ${work_dir}/third-stage
@@ -291,49 +146,22 @@ rm -f /usr/bin/dpkg
 EOF
 
 # Run third stage
-chmod 0755 ${work_dir}/third-stage
+chmod 0755 "${work_dir}"/third-stage
+status "Run third stage"
 systemd-nspawn_exec /third-stage
 
-# Clean up eatmydata
-systemd-nspawn_exec dpkg-divert --remove --rename /usr/bin/dpkg
 
 # Clean system
-systemd-nspawn_exec << 'EOF'
-rm -f /0
-rm -rf /bsp
-fc-cache -frs
-rm -rf /tmp/*
-rm -rf /etc/*-
-rm -rf /hs_err*
-rm -rf /userland
-rm -rf /opt/vc/src
-rm -f /etc/ssh/ssh_host_*
-rm -rf /var/lib/dpkg/*-old
-rm -rf /var/lib/apt/lists/*
-rm -rf /var/cache/apt/*.bin
-rm -rf /var/cache/apt/archives/*
-rm -rf /var/cache/debconf/*.data-old
-for logs in $(find /var/log -type f); do > $logs; done
-history -c
-EOF
+include clean_system
+trap clean_build ERR SIGTERM SIGINT
 
-# Disable the use of http proxy in case it is enabled
-if [ -n "$proxy_url" ]; then
-  unset http_proxy
-  rm -rf ${work_dir}/etc/apt/apt.conf.d/66proxy
-fi
 
+# Disable the use of http proxy in case it is enabled.
+disable_proxy
 # Mirror & suite replacement
-if [[ ! -z "${4}" || ! -z "${5}" ]]; then
-  mirror=${4}
-  suite=${5}
-fi
-
-# Define sources.list
-cat << EOF > ${work_dir}/etc/apt/sources.list
-deb ${mirror} ${suite} ${components//,/ }
-#deb-src ${mirror} ${suite} ${components//,/ }
-EOF
+restore_mirror
+# Reload sources.list
+include sources.list
 
 # We need an older gcc because of kernel age
 cd "${base_dir}"
@@ -432,17 +260,17 @@ ln -s /usr/src/kernel build
 ln -s /usr/src/kernel source
 cd "${base_dir}"
 
-# Calculate the space to create the image
-root_size=$(du -s -B1 ${work_dir} --exclude=${work_dir}/boot | cut -f1)
-root_extra=$((${root_size}/1024/1000*5*1024/5))
-raw_size=$(($((${free_space}*1024))+${root_extra}+$((${bootsize}*1024))+4096))
 
-# Create the disk and partition it
-echo "Creating image file ${image_name}.img"
-fallocate -l $(echo ${raw_size}Ki | numfmt --from=iec-i --to=si) "${image_dir}/${image_name}.img"
+# Calculate the space to create the image and create
+make_image
+
+# Create the disk partitions
+status "Create the disk partitions"
 parted -s "${image_dir}/${image_name}.img" mklabel msdos
-parted -s "${image_dir}/${image_name}.img" mkpart primary ext3 1MiB ${bootsize}MiB
-parted -s -a minimal "${image_dir}/${image_name}.img" mkpart primary $fstype ${bootsize}MiB 100%
+parted -s "${image_dir}/${image_name}.img" mkpart primary fat32 1MiB "${bootsize}"MiB
+parted -s -a minimal "${image_dir}/${image_name}.img" mkpart primary "$fstype" "${bootsize}"MiB 100%
+
+
 
 # Set the partition variables
 loopdevice=`losetup -f --show ${current_dir}/${image_name}.img`
@@ -453,6 +281,7 @@ bootp=${device}p1
 rootp=${device}p2
 
 # Create file systems
+status "Formatting partitions" green
 mkfs.ext3 -L BOOT ${bootp}
 if [[ $fstype == ext4 ]]; then
   features="-O ^64bit,^metadata_csum"
@@ -462,6 +291,7 @@ fi
 mkfs $features -t $fstype -L ROOTFS ${rootp}
 
 # Create the dirs for the partitions and mount them
+status "Create the dirs for the partitions and mount them"
 mkdir -p "${base_dir}"/root
 mount ${rootp} "${base_dir}"/root
 mkdir -p "${base_dir}"/root/boot
@@ -522,42 +352,13 @@ sync
 
 cd "${base_dir}"
 
+# Remove loop devices
 losetup -d ${loopdevice}
 
-# Limit CPU function
-limit_cpu (){
-  rand=$(tr -cd 'A-Za-z0-9' < /dev/urandom | head -c4 ; echo) # Random name group
-  cgcreate -g cpu:/cpulimit-${rand} # Name of group cpulimit
-  cgset -r cpu.shares=800 cpulimit-${rand} # Max 1024
-  cgset -r cpu.cfs_quota_us=80000 cpulimit-${rand} # Max 100000
-  # Retry command
-  local n=1; local max=5; local delay=2
-  while true; do
-    cgexec -g cpu:cpulimit-${rand} "$@" && break || {
-      if [[ $n -lt $max ]]; then
-        ((n++))
-        echo -e "\e[31m Command failed. Attempt $n/$max \033[0m"
-        sleep $delay;
-      else
-        echo "The command has failed after $n attempts."
-        break
-      fi
-    }
-  done
-}
 
-if [ $compress = xz ]; then
-  if [ $(arch) == 'x86_64' ]; then
-    echo "Compressing ${image_name}.img"
-    [ $(nproc) \< 3 ] || cpu_cores=3 # cpu_cores = Number of cores to use
-    limit_cpu pixz -p ${cpu_cores:-2} "${image_dir}/${image_name}.img" # -p Nº cpu cores use
-    chmod 0644 ${current_dir}/${image_name}.img.xz
-  fi
-else
-  chmod 0644 "${image_dir}/${image_name}.img"
-fi
+# Compress image compilation
+include compress_img
 
-# Clean up all the temporary build stuff and remove the directories
-# Comment this out to keep things around if you want to see what may have gone wrong
-echo "Clean up the build system"
-rm -rf "${base_dir}"
+# Clean up all the temporary build stuff and remove the directories.
+# Comment this out to keep things around if you want to see what may have gone wrong.
+clean_build
