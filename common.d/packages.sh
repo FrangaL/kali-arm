b8baca1608d2daa3518d552c6179bfb782741647
Steev Klimaszewski <steev@kali.org>
2021-09-23 21:16:03 -0500

    common: packages: alphabetize and add vim

--- common.d/packages.sh
+++ common.d/packages.sh
@@ -8,9 +8,9 @@ debootstrap_base="kali-archive-keyring,eatmydata"
 minimal_pkgs="ca-certificates iw parted ssh wpasupplicant sudo"
 
 # This is the list of minimal common packages
-common_min_pkgs="apt-transport-https firmware-linux firmware-realtek firmware-atheros \
-firmware-libertas ifupdown initramfs-tools kali-defaults man-db mlocate netcat-traditional net-tools \
-parted pciutils psmisc rfkill screen snmpd snmp sudo tftp tmux unrar usbutils zerofree crda iw"
+common_min_pkgs="apt-transport-https crda firmware-linux firmware-realtek firmware-atheros \
+firmware-libertas ifupdown initramfs-tools iw kali-defaults man-db mlocate netcat-traditional net-tools \
+parted pciutils psmisc rfkill screen snmpd snmp sudo tftp tmux unrar usbutils vim zerofree"
 # This is the list of common packages
 common_pkgs="kali-linux-core apt-transport-https bluez bluez-firmware dialog \
 ifupdown initramfs-tools inxi iw libnss-systemd man-db mlocate net-tools network-manager crda \
