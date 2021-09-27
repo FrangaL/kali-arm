62eed772c7ad60842c197838e09c24a32ba9fc21
Ben Wilson <g0tmi1k@kali.org>
2021-09-27 12:35:06 +0100

    Add base_image & third_stage

---
+++ common.d/base_image.sh
@@ -0,0 +1,129 @@
+#!/usr/bin/env bash
+#
+# Every Kali ARM image starts with this
+#
+
+# Stop on error
+set -e
+
+# Say where we are
+log "base_image" green
+
+# shellcheck disable=SC2154
+# Load general functions
+# shellcheck source=/dev/null
+source ./common.d/functions.sh
+
+# Load common variables
+include variables
+# Checks script environment
+include check
+# Packages build list
+include packages
+# Execute initial debootstrap
+debootstrap_exec http://http.kali.org/kali
+# Enable eatmydata in compilation
+include eatmydata
+# debootstrap second stage
+systemd-nspawn_exec eatmydata /debootstrap/debootstrap --second-stage
+# Define sources.list
+include sources.list
+# APT options
+include apt_options
+# So X doesn't complain, we add kali to hosts
+include hosts
+# Set hostname
+set_hostname "${hostname}"
+# Network configs
+include network
+
+# Copy directory bsp into build dir
+status "Copy directory bsp into build dir"
+cp -rp bsp "${work_dir}"
+
+# Third stage
+cat <<EOF > "${work_dir}"/third-stage
+#!/usr/bin/env bash
+set -e
+status_3i=0
+status_3t=\$(grep '^status_stage3 ' \$0 | wc -l)
+
+status_stage3() {
+  status_3i=\$((status_3i+1))
+  echo  " [i] Stage 3 (\${status_3i}/\${status_3t}): \$1"
+}
+
+status_stage3 'Update apt'
+export DEBIAN_FRONTEND=noninteractive
+eatmydata apt-get update
+
+status_stage3 'Install core packages'
+eatmydata apt-get -y install ${third_stage_pkgs}
+
+status_stage3 'Install packages'
+eatmydata apt-get install -y ${packages} || eatmydata apt-get install -y --fix-broken
+
+status_stage3 'ntp doesn't always sync the date, but systemd's timesyncd does, so we remove ntp and reinstall it with this'
+eatmydata apt-get install -y systemd-timesyncd --autoremove
+
+status_stage3 'Clean up'
+eatmydata apt-get -y --purge autoremove
+
+status_stage3 'Linux console/keyboard configuration'
+echo 'console-common console-data/keymap/policy select Select keymap from full list' | debconf-set-selections
+echo 'console-common console-data/keymap/full select en-latin1-nodeadkeys' | debconf-set-selections
+
+status_stage3 'Copy all services'
+cp -p /bsp/services/all/*.service /etc/systemd/system/
+
+status_stage3 'Enable SSH service'
+systemctl enable ssh
+
+status_stage3 'Generate SSH host keys on first run'
+systemctl enable regenerate_ssh_host_keys
+
+status_stage3 'Enabling ssh by putting ssh or ssh.txt file in /boot'
+systemctl enable enable-ssh
+
+status_stage3 'Copy script growpart'
+install -m755 /bsp/scripts/growpart /usr/local/bin/
+
+status_stage3 'Copy script rpi-resizerootfs'
+install -m755 /bsp/scripts/rpi-resizerootfs /usr/sbin/
+
+status_stage3 'Enable rpi-resizerootfs first boot'
+systemctl enable rpi-resizerootfs
+
+status_stage3 'Enable runonce'
+install -m755 /bsp/scripts/runonce /usr/sbin/
+cp -rf /bsp/runonce.d /etc
+systemctl enable runonce
+
+status_stage3 'Set a REGDOMAIN'
+# This needs to be done or wireless doesnt work correctly on the RPi 3B+
+sed -i -e 's/REGDOM.*/REGDOMAIN=00/g' /etc/default/crda || true
+
+status_stage3 'Try and make the console a bit nicer. Set the terminus font for a bit nicer display'
+sed -i -e 's/FONTFACE=.*/FONTFACE="Terminus"/' /etc/default/console-setup
+sed -i -e 's/FONTSIZE=.*/FONTSIZE="6x12"/' /etc/default/console-setup
+
+status_stage3 'Fix startup time from 5 minutes to 15 secs on raise interface wlan0'
+sed -i 's/^TimeoutStartSec=5min/TimeoutStartSec=15/g' "/usr/lib/systemd/system/networking.service"
+
+status_stage3 'Disable haveged daemon'
+systemctl disable haveged
+
+status_stage3 'Enable login over serial (No password)'
+echo "T0:23:respawn:/sbin/agetty -L ttyAMA0 115200 vt100" >> /etc/inittab
+EOF
+
+if [ "${desktop}" != "none" ]; then
+  log "Desktop mode enabled: ${desktop}" green
+  cat <<EOF >> "${work_dir}"/third-stage
+status_stage3 'Install desktop packages'
+eatmydata apt-get install -y ${desktop_pkgs} ${extra} || eatmydata apt-get install -y --fix-broken
+
+status_stage3 'Allow users to use NetworkManager over ssh'
+install -m644 /bsp/polkit/10-NetworkManager.pkla /var/lib/polkit-1/localauthority/50-local.d
+EOF
+fi
--- rpi.sh
+++ rpi.sh
@@ -7,14 +7,6 @@
 # More information: https://www.kali.org/docs/arm/raspberry-pi-2/
 #
 
-# Stop on error
-set -e
-
-# shellcheck disable=SC2154
-# Load general functions
-# shellcheck source=/dev/null
-source ./common.d/functions.sh
-
 # Hardware model
 hw_model=${hw_model:-"rpi"}
 # Architecture
@@ -24,71 +16,16 @@ variant=${variant:-"${architecture}"}
 # Desktop manager (xfce, gnome, i3, kde, lxde, mate, e17 or none)
 desktop=${desktop:-"xfce"}
 
-# Load common variables
-include variables
-# Checks script environment
-include check
-# Packages build list
-include packages
-# Execute initial debootstrap
-debootstrap_exec http://http.kali.org/kali
-# Enable eatmydata in compilation
-include eatmydata
-# debootstrap second stage
-systemd-nspawn_exec eatmydata /debootstrap/debootstrap --second-stage
-# Define sources.list
-include sources.list
-# APT options
-include apt_options
-# So X doesn't complain, we add kali to hosts
-include hosts
-# Set hostname
-set_hostname "${hostname}"
+# Load default base_image configs
+include base_image
+
 # Network configs
 include network
 add_interface eth0
 
-# Copy directory bsp into build dir
-status "Copy directory bsp into build dir"
-cp -rp bsp "${work_dir}"
-
 # Third stage
-cat <<EOF > "${work_dir}"/third-stage
-#!/usr/bin/env bash
-set -e
-status_3i=0
-status_3t=\$(grep '^status_stage3 ' \$0 | wc -l)
-
-status_stage3() {
-  status_3i=\$((status_3i+1))
-  echo  " [i] Stage 3 (\${status_3i}/\${status_3t}): \$1"
-}
-
-status_stage3 'Update apt'
-export DEBIAN_FRONTEND=noninteractive
-eatmydata apt-get update
-
-status_stage3 'Install core packages'
-eatmydata apt-get -y install ${third_stage_pkgs}
-
-status_stage3 'Install packages'
-eatmydata apt-get install -y ${packages} || eatmydata apt-get install -y --fix-broken
-
-status_stage3 'Install desktop packages'
-eatmydata apt-get install -y ${desktop_pkgs} ${extra} || eatmydata apt-get install -y --fix-broken
-
-status_stage3 'ntp doesn't always sync the date, but systemd's timesyncd does, so we remove ntp and reinstall it with this'
-eatmydata apt-get install -y systemd-timesyncd --autoremove
-
-status_stage3 'Clean up'
-eatmydata apt-get -y --purge autoremove
-
-status_stage3 'Linux console/keyboard configuration'
-echo 'console-common console-data/keymap/policy select Select keymap from full list' | debconf-set-selections
-echo 'console-common console-data/keymap/full select en-latin1-nodeadkeys' | debconf-set-selections
-
-status_stage3 'Copy all services'
-cp -p /bsp/services/all/*.service /etc/systemd/system/
+cat <<EOF >> "${work_dir}"/third-stage
+status_stage3 'Copy rpi services'
 cp -p /bsp/services/rpi/*.service /etc/systemd/system/
 
 status_stage3 'Script mode wlan monitor START/STOP'
@@ -101,61 +38,15 @@ wget -qO /etc/apt/trusted.gpg.d/kali_pi-archive-keyring.gpg https://re4son-kerne
 eatmydata apt-get update
 eatmydata apt-get install -y ${re4son_pkgs}
 
-status_stage3 'Copy script rpi-resizerootfs'
-install -m755 /bsp/scripts/rpi-resizerootfs /usr/sbin/
-install -m755 /bsp/scripts/growpart /usr/local/bin/
-
 status_stage3 'Copy script for handling wpa_supplicant file'
 install -m755 /bsp/scripts/copy-user-wpasupplicant.sh /usr/bin/
 
-status_stage3 'Enable rpi-resizerootfs first boot'
-systemctl enable rpi-resizerootfs
-
-status_stage3 'Generate SSH host keys on first run'
-systemctl enable regenerate_ssh_host_keys
-
-status_stage3 'Enable ssh'
-systemctl enable ssh
-
 status_stage3 'Enable copying of user wpa_supplicant.conf file'
 systemctl enable copy-user-wpasupplicant
-
-status_stage3 'Enabling ssh by putting ssh or ssh.txt file in /boot'
-systemctl enable enable-ssh
-
-status_stage3 'Allow users to use NetworkManager over ssh'
-install -m644 /bsp/polkit/10-NetworkManager.pkla /var/lib/polkit-1/localauthority/50-local.d
-
-status_stage3 'Set a REGDOMAIN. This needs to be done or wireless doesnt work correctly on the RPi 3B+'
-sed -i -e 's/REGDOM.*/REGDOMAIN=00/g' /etc/default/crda
-
-status_stage3 'Enable login over serial'
-echo "T0:23:respawn:/sbin/agetty -L ttyAMA0 115200 vt100" >> /etc/inittab
-
-status_stage3 'Try and make the console a bit nicer. Set the terminus font for a bit nicer display'
-sed -i -e 's/FONTFACE=.*/FONTFACE="Terminus"/' /etc/default/console-setup
-sed -i -e 's/FONTSIZE=.*/FONTSIZE="6x12"/' /etc/default/console-setup
-
-status_stage3 'Fix startup time from 5 minutes to 15 secs on raise interface wlan0'
-sed -i 's/^TimeoutStartSec=5min/TimeoutStartSec=15/g' "/usr/lib/systemd/system/networking.service"
-
-status_stage3 'Disable haveged daemon'
-systemctl disable haveged
-
-status_stage3 'Enable runonce'
-install -m755 /bsp/scripts/runonce /usr/sbin/
-cp -rf /bsp/runonce.d /etc
-systemctl enable runonce
-
-status_stage3 'Clean up dpkg.eatmydata'
-rm -f /usr/bin/dpkg
-dpkg-divert --remove --rename /usr/bin/dpkg
 EOF
 
 # Run third stage
-chmod 0755 "${work_dir}"/third-stage
-status "Run third stage"
-systemd-nspawn_exec /third-stage
+include third_stage
 
 # Configure Raspberry Pi firmware (set config.txt to 64-bit)
 include rpi_firmware
