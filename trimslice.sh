a42a348db5f0e5366f324f403196cdc74b4bdbbe
Fran <frangal@gmail.com>
2021-11-04 18:25:51 +0100

    trimslice.sh fstab convert

--- trimslice.sh
+++ trimslice.sh
@@ -40,17 +40,6 @@ include third_stage
 
 # Clean system
 include clean_system
-trap clean_build ERR SIGTERM SIGINT
-
-# systemd doesn't seem to be generating the fstab properly for some people, so
-# let's create one. We add the root partition below after creating the image
-# as we don't know the UUID until the image has been partitioned
-status 'Create /etc/fstab'
-cat << EOF > ${work_dir}/etc/fstab
-# <file system> <mount point>   <type>  <options>       <dump>  <pass>
-proc            /proc           proc    defaults          0       0
-LABEL=BOOT  /boot           ext2    defaults          0       2
-EOF
 
 # Calculate the space to create the image and create
 make_image
@@ -63,12 +52,9 @@ parted -s "${image_dir}/${image_name}.img" mkpart primary ext2 1MiB ${bootsize}M
 parted -s -a minimal "${image_dir}/${image_name}.img" mkpart primary $fstype ${bootsize}MiB 100%
 
 # Set the partition variables
-loopdevice=$(losetup -f --show "${image_dir}/${image_name}.img")
-device=$(kpartx -va ${loopdevice} | sed 's/.*\(loop[0-9]\+\)p.*/\1/g' | head -1)
-sleep 5s
-device="/dev/mapper/${device}"
-bootp=${device}p1
-rootp=${device}p2
+loopdevice=$(losetup --show -fP "${image_dir}/${image_name}.img")
+bootp="${loopdevice}p1"
+rootp="${loopdevice}p2"
 
 mkfs.ext2 -L BOOT "${bootp}"
 if [[ $fstype == ext4 ]]; then
@@ -76,7 +62,10 @@ if [[ $fstype == ext4 ]]; then
 elif [[ $fstype == ext3 ]]; then
   features="^64bit"
 fi
-mkfs -O "$features" -t "$fstype" -L ROOTFS "${rootp}"
+mkfs -U "$root_uuid" -O "$features" -t "$fstype" -L ROOTFS "${rootp}"
+
+# Make fstab.
+make_fstab
 
 # Create the dirs for the partitions and mount them
 status "Create the dirs for the partitions and mount them"
@@ -85,15 +74,10 @@ mount ${rootp} "${base_dir}"/root
 mkdir -p "${base_dir}"/root/boot
 mount ${bootp} "${base_dir}"/root/boot
 
-# Create an fstab so that we don't mount / read-only
-status "Fix rootfs entry in /etc/fstab"
-UUID=$(blkid -s UUID -o value ${rootp})
-echo "UUID=$UUID /               $fstype    errors=remount-ro 0       1" >> ${work_dir}/etc/fstab
-
 status "Fix root entry in extlinux.conf"
 # Ensure we don't have root=/dev/sda3 in the extlinux.conf which comes from running u-boot-menu in a cross chroot
 # We do this down here because we don't know the UUID until after the image is created
-sed -i -e "0,/root=.*/s//root=UUID=$(blkid -s UUID -o value ${rootp}) rootfstype=$fstype console=ttyS0,115200 console=tty1 consoleblank=0 rw quiet rootwait/g" ${work_dir}/boot/extlinux/extlinux.conf
+sed -i -e "0,/root=.*/s//root=UUID=$root_uuid rootfstype=$fstype console=ttyS0,115200 console=tty1 consoleblank=0 rw quiet rootwait/g" ${work_dir}/boot/extlinux/extlinux.conf
 # And we remove the "Debian GNU/Linux because we're Kali"
 sed -i -e "s/Debian GNU\/Linux/Kali Linux/g" ${work_dir}/boot/extlinux/extlinux.conf
 
