61ef2f4a71b19661c57aed237256f1dd2c1cf2ab
Fran <frangal@gmail.com>
2021-11-05 19:20:07 +0100

    Migrate nanopi2.sh

--- nanopi2.sh
+++ nanopi2.sh
@@ -7,155 +7,33 @@
 # More information: https://www.kali.org/docs/arm/nanopi2/
 #
 
-# Stop on error
-set -e
-
-# Uncomment to activate debug
-# debug=true
-
-# shellcheck disable=SC2154
-# Load general functions
-# shellcheck source=/dev/null
-source ./common.d/functions.sh
-
-
 # Hardware model
 hw_model=${hw_model:-"nanopi2"}
-# Architecture
+# Architecture (arm64, armhf, armel)
 architecture=${architecture:-"armhf"}
 # Desktop manager (xfce, gnome, i3, kde, lxde, mate, e17 or none)
 desktop=${desktop:-"xfce"}
 
-# Load common variables
-include variables
-# Checks script environment
-include check
-# Packages build list
-include packages
-# Execute initial debootstrap
-debootstrap_exec http://http.kali.org/kali
-# Enable eatmydata in compilation
-include eatmydata
-# debootstrap second stage
-systemd-nspawn_exec eatmydata /debootstrap/debootstrap --second-stage
-# Define sources.list
-include sources.list
-# APT options
-include apt_options
-# So X doesn't complain, we add kali to hosts
-include hosts
-# Set hostname
-set_hostname "${hostname}"
+# Load default base_image configs
+source ./common.d/base_image.sh
+
 # Network configs
-include network
 add_interface eth0
 
-# Copy directory bsp into build dir
-status "Copy directory bsp into build dir"
-cp -rp bsp "${work_dir}"
-
-# Disable RESUME (suspend/resume is currently broken anyway!) which speeds up boot massively
-mkdir -p ${work_dir}/etc/initramfs-tools/conf.d/
-cat << EOF > ${work_dir}/etc/initramfs-tools/conf.d/resume
-RESUME=none
-EOF
-
-
 # Third stage
-cat <<EOF > "${work_dir}"/third-stage
-#!/usr/bin/env bash
-set -e
-status_3i=0
-status_3t=\$(grep '^status_stage3 ' \$0 | wc -l)
-
-status_stage3() {
-  status_3i=\$((status_3i+1))
-  echo  " [i] Stage 3 (\${status_3i}/\${status_3t}): \$1"
-}
-
-status_stage3 'Update apt'
-export DEBIAN_FRONTEND=noninteractive
-eatmydata apt-get update
-
-status_stage3 'Install core packages'
-eatmydata apt-get -y install ${third_stage_pkgs}
-
-status_stage3 'Install packages'
-eatmydata apt-get install -y ${packages} || eatmydata apt-get install -y --fix-broken
-
-status_stage3 'Install desktop packages'
-eatmydata apt-get install -y ${desktop_pkgs} ${extra} || eatmydata apt-get install -y --fix-broken
-
-status_stage3 'ntp does not always sync the date, but systemd-timesyncd does, so we remove ntp and reinstall it with this'
-eatmydata apt-get install -y systemd-timesyncd --autoremove
-
-status_stage3 'Clean up'
-eatmydata apt-get -y --purge autoremove
-
-status_stage3 'Linux console/keyboard configuration'
-echo 'console-common console-data/keymap/policy select Select keymap from full list' | debconf-set-selections
-echo 'console-common console-data/keymap/full select en-latin1-nodeadkeys' | debconf-set-selections
-
-status_stage3 'Copy all services'
-cp -p /bsp/services/all/*.service /etc/systemd/system/
-
-status_stage3 'Copy script rpi-resizerootfs'
-install -m755 /bsp/scripts/rpi-resizerootfs /usr/sbin/
-install -m755 /bsp/scripts/growpart /usr/local/bin/
-
-status_stage3 'Enable rpi-resizerootfs first boot'
-systemctl enable rpi-resizerootfs
-
-status_stage3 'Generate SSH host keys on first run'
-systemctl enable regenerate_ssh_host_keys
-status_stage3 'Enable sshd'
-systemctl enable ssh
-
-status_stage3 'Allow users to use NetworkManager over ssh'
-install -m644 /bsp/polkit/10-NetworkManager.pkla /var/lib/polkit-1/localauthority/50-local.d
-
-status_stage3 'Install ca-certificate'
-cd /root
-apt download -o APT::Sandbox::User=root ca-certificates 2>/dev/null
-
-status_stage3 'Set a REGDOMAIN'
-# sometimes the package is not installed, so we ignore the error
-sed -i -e 's/REGDOM.*/REGDOMAIN=00/g' /etc/default/crda || true
-
+cat <<EOF >> "${work_dir}"/third-stage
 status_stage3 'Enable login over serial'
 echo "T0:23:respawn:/sbin/agetty -L ttyAMA0 115200 vt100" >> /etc/inittab
-
-status_stage3 'Try and make the console a bit nicer. Set the terminus font for a bit nicer display'
-sed -i -e 's/FONTFACE=.*/FONTFACE="Terminus"/' /etc/default/console-setup
-sed -i -e 's/FONTSIZE=.*/FONTSIZE="6x12"/' /etc/default/console-setup
-
-status_stage3 'Fix startup time from 5 minutes to 15 secs on raise interface wlan0'
-sed -i 's/^TimeoutStartSec=5min/TimeoutStartSec=15/g' "/usr/lib/systemd/system/networking.service"
-
-status_stage3 'Enable runonce'
-install -m755 /bsp/scripts/runonce /usr/sbin/
-cp -rf /bsp/runonce.d /etc
-systemctl enable runonce
-
-status_stage3 'Clean up dpkg.eatmydata'
-rm -f /usr/bin/dpkg
-dpkg-divert --remove --rename /usr/bin/dpkg
 EOF
 
 # Run third stage
-chmod 0755 "${work_dir}"/third-stage
-status "Run third stage"
-systemd-nspawn_exec /third-stage
+include third_stage
 
 # Clean system
 include clean_system
 
-# Disable the use of http proxy in case it is enabled.
-disable_proxy
-# Mirror & suite replacement
-restore_mirror
-# Reload sources.list
-include sources.list
+# Calculate the space to create the image and create
+make_image
 
 # We need an older gcc because of kernel age
 cd "${base_dir}"
@@ -270,14 +148,14 @@ bootp="${loopdevice}p1"
 rootp="${loopdevice}p2"
 
 # Create file systems
-status "Formatting partitions" green
-mkfs.ext2 -L BOOT ${bootp}
-if [[ $fstype == ext4 ]]; then
-  features="-O ^64bit,^metadata_csum"
-elif [[ $fstype == ext3 ]]; then
-  features="-O ^64bit"
+status "Formatting partitions"
+mkfs.ext2 -L BOOT "${bootp}"
+if [[ "$fstype" == "ext4" ]]; then
+  features="^64bit,^metadata_csum"
+elif [[ "$fstype" == "ext3" ]]; then
+  features="^64bit"
 fi
-mkfs -U "$root_uuid" $features -t $fstype -L ROOTFS ${rootp}
+mkfs -U "$root_uuid" -O "$features" -t "$fstype" -L ROOTFS "${rootp}"
 
 # Make fstab.
 make_fstab
@@ -291,12 +169,6 @@ mount ${bootp} "${base_dir}"/root/boot
 
 echo "Rsyncing rootfs into image file"
 rsync -HPavz -q ${work_dir}/ ${base_dir}/root/
-
-# Unmount partitions
-sync
-umount -l ${bootp}
-umount -l ${rootp}
-kpartx -dv ${loopdevice}
 
 # Samsung bootloaders must be signed
 # These are the same steps that are done by
@@ -337,12 +209,5 @@ sync
 
 cd "${repo_dir}/"
 
-# Remove loop devices
-losetup -d ${loopdevice}
-
-# Compress image compilation
-include compress_img
-
-# Clean up all the temporary build stuff and remove the directories.
-# Comment this out to keep things around if you want to see what may have gone wrong.
-clean_build
+# Load default finish_image configs
+include finish_image
