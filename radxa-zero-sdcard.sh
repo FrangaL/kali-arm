336b991a3ec762692fd13b148c04390b83be7a85
Steev Klimaszewski <steev@kali.org>
2021-12-03 13:46:24 -0600

    rzero: updates for building sdcard image
    We now use the vanilla 5.10 tree and patch in the radxa zero patches on top.
    Also included are 2 hacks, one to remove a shutdown callback that fixes rebooting the devices, as well as one that gets rid of an error early in boot, and finally a patch to recognize the processor revision.
    
    Update the firmware to the newest 43456 that I could find (comes rpi-distro/firmware-nonfree, aka the firmware from the RaspberryPi 400)
    
    Also add in a script that generates a uInitrd after creating the initrd, so that u-boot will load it properly.

--- radxa-zero-sdcard.sh
+++ radxa-zero-sdcard.sh
@@ -21,8 +21,8 @@ source ./common.d/base_image.sh
 
 # Third stage
 cat <<EOF >> "${work_dir}"/third-stage
-status_stage3 'Install u-boot-menu'
-eatmydata apt-get install -y u-boot-menu
+status_stage3 'Install u-boot tools'
+eatmydata apt-get install -y u-boot-menu u-boot-tools
 
 # Note: This just creates an empty /boot/extlinux/extlinux.conf for us to use 
 # later when we install the kernel, and then fixup further down
@@ -30,8 +30,14 @@ status_stage3 'Run u-boot-update'
 u-boot-update
 
 status_stage3 'Copy WiFi/BT firmware'
+# Protip: Can actually use the same firmware as the Pi400
 mkdir -p /lib/firmware/brcm/
 cp /bsp/firmware/radxa-zero/* /lib/firmware/brcm/
+rm /lib/firmware/brcm/99-uboot
+
+status_stage3 'Add post update initramfs script to generate uInitrd'
+mkdir -p /etc/initramfs/post-update.d/
+install -m755 /bsp/firmware/radxa-zero/99-uboot /etc/initramfs/post-update.d/
 EOF
 
 # Run third stage
@@ -44,14 +50,37 @@ trap clean_build ERR SIGTERM SIGINT
 # Kernel section. If you want to use a custom kernel, or configuration, replace
 # them in this section
 status "Kernel stuff"
-git clone --depth 1 -b linux-5.10.y-radxa-zero https://github.com/radxa/kernel.git ${work_dir}/usr/src/kernel
-cd ${work_dir}/usr/src/kernel
-git rev-parse HEAD > ${work_dir}/usr/src/kernel-at-commit
+# Vendor kernel
+#git clone --depth 1 -b linux-5.10.y-radxa-zero https://github.com/radxa/kernel.git ${work_dir}/usr/src/linux
+# Upstream stable 5.10
+git clone --depth 1 -b linux-5.10.y git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git ${work_dir}/usr/src/linux
+# Upstream 5.15
+#git clone --depth 1 -b linux-5.15.y git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git ${work_dir}/usr/src/linux
+cd ${work_dir}/usr/src/linux
 rm -rf .git
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
-patch -p1 --no-backup-if-mismatch < ${repo_dir}/patches/kali-wifi-injection-5.9.patch
-patch -p1 --no-backup-if-mismatch < ${repo_dir}/patches/0001-wireless-carl9170-Enable-sniffer-mode-promisc-flag-t.patch
+patch -Np1 -i "${repo_dir}/patches/kali-wifi-injection-5.9.patch"
+patch -Np1 -i "${repo_dir}/patches/0001-wireless-carl9170-Enable-sniffer-mode-promisc-flag-t.patch"
+# These patches are only for 5.10
+# Patches 0001-0015 are already in the vendor kernel, so if using that, only 0015-0017 need to be applied.
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0001-arm64-dts-amlogic-add-support-for-Radxa-Zero.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0002-arm64-configs-add-radxa_zero_defconfig.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0003-add-overlay-compilation-support.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0004-arm64-dts-Radxa-Zero-set-aliases-for-serial-and-i2c.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0005-add-meson-overlay-support.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0006-arm64-dts-Radxa-Zero-set-aliases-for-spi.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0007-arm64-dts-add-meson-i2c-and-spi-overlay-support.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0008-arm64-radxa_zero_defconfig-disable-ARCH_ROCKCHIP.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0009-arm64-dts-amlogic-meson-g12-common-add-uart_AO_B-pin.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0010-arm64-dts-amlogic-overlay-add-support-for-uart_AO_B.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0011-arm64-dts-amlogic-overlay-use-uart_AO-in-dtbo-way-in.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0012-arm64-dts-radxa-zero-set-dr_mode-of-usb-node-to-otg.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0013-arm64-dts-radxa-zero-remove-dai-link-0-node.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0014-arm64-dts-radxa-zero-add-user-led.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0015-HACK-of-partial-revert-of-fdt.c-changes.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0016-Add-the-SOC-ID-for-the-S905Y2-used-in-the-Radxa-Zero.patch"
+patch -Np1 -i "${repo_dir}/patches/radxa-zero/0017-HACK-remove-shutdown-callback-to-fix-reboot.patch"
 make radxa_zero_defconfig
 make -j $(grep -c processor /proc/cpuinfo) LOCALVERSION="" bindeb-pkg
 make mrproper
@@ -82,7 +111,7 @@ rootp="${loopdevice}p1"
 
 # Create file systems
 status "Formatting partitions"
-mkfs.ext2 ${rootp}
+mkfs.ext4 ${rootp}
 
 # Create the dirs for the partitions and mount them
 status "Create the dirs for the partitions and mount them"
@@ -109,22 +138,22 @@ status "Rsyncing rootfs into image file"
 rsync -HPavz -q "${work_dir}"/ "${base_dir}"/root/
 sync
 
-status "u-Boot"
-cd "${work_dir}"
-git clone https://github.com/radxa/fip.git
-git clone https://github.com/radxa/u-boot.git --depth 1 -b radxa-zero-v2021.07
-cd u-boot
-make distclean
-make radxa-zero_config
-make ARCH=arm -j$(nproc)
-cp u-boot.bin ../fip/radxa-zero/bl33.bin
-cd ../fip/radxa-zero/
-make
+#status "u-Boot"
+#cd "${work_dir}"
+#git clone https://github.com/radxa/fip.git
+#git clone https://github.com/radxa/u-boot.git --depth 1 -b radxa-zero-v2021.07
+#cd u-boot
+#make distclean
+#make radxa-zero_config
+#make ARCH=arm -j$(nproc)
+#cp u-boot.bin ../fip/radxa-zero/bl33.bin
+#cd ../fip/radxa-zero/
+##make
 # https://wiki.radxa.com/Zero/dev/u-boot
-dd if=u-boot.bin.sd.bin of=${loopdevice} conv=fsync,notrunc bs=1 count=442
-dd if=u-boot.bin.sd.bin of=${loopdevice} conv=fsync,notrunc bs=512 skip=1 seek=1
-cd "${repo_dir}/"
-rm -rf "${work_dir}"/{fip,u-boot}
+#dd if=u-boot.bin.sd.bin of=${loopdevice} conv=fsync,notrunc bs=1 count=442
+#dd if=u-boot.bin.sd.bin of=${loopdevice} conv=fsync,notrunc bs=512 skip=1 seek=1
+#cd "${repo_dir}/"
+#rm -rf "${work_dir}"/{fip,u-boot}
 
 # Load default finish_image configs
 include finish_image
