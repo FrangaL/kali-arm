05175d40750cbb873e014b195464c56bfe855a12
Steev Klimaszewski <steev@kali.org>
2022-04-13 22:20:24 -0500

    usbarmory: Fix the builds
    The kernel was bumped to 5.15, so we patch the 5.15 wifi injection in.
    Also, the defconfig file needs to be named _defconfig for copying it in and using it as a target.
    With newer u-boot, we don't seem to need the patch for usbarmory mki
    Patch from upstream for mkii u-boot requires newer u-boot as well, so bump it.

--- usb-armory-mki.sh
+++ usb-armory-mki.sh
@@ -107,23 +107,23 @@ touch .scmversion
 export ARCH=arm
 export CROSS_COMPILE=arm-none-eabi-
 #patch -p1 --no-backup-if-mismatch < ${repo_dir}/patches/ARM-drop-cc-option-fallbacks-for-architecture-select.patch
-patch -p1 --no-backup-if-mismatch < ${repo_dir}/patches/kali-wifi-injection-5.4.patch
+patch -p1 --no-backup-if-mismatch < ${repo_dir}/patches/kali-wifi-injection-5.15.patch
 patch -p1 --no-backup-if-mismatch < ${repo_dir}/patches/0001-wireless-carl9170-Enable-sniffer-mode-promisc-flag-t.patch
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-gpio.dts -O arch/arm/boot/dts/imx53-usbarmory-gpio.dts
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-host.dts -O arch/arm/boot/dts/imx53-usbarmory-host.dts
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-i2c.dts -O arch/arm/boot/dts/imx53-usbarmory-i2c.dts
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-scc2.dts -O arch/arm/boot/dts/imx53-usbarmory-scc2.dts
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-spi.dts -O arch/arm/boot/dts/imx53-usbarmory-spi.dts
-wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/usbarmory_linux-5.15.defconfig -O ../usbarmory_linux-5.15.defconfig
-cp ../usbarmory_linux-5.15.defconfig arch/arm/configs/
-make usbarmory_linux-5.15.defconfig
+wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/usbarmory_linux-5.15.defconfig -O ../usbarmory_linux-5.15_defconfig
+cp ../usbarmory_linux-5.15_defconfig arch/arm/configs/
+make usbarmory_linux-5.15_defconfig
 make LOADADDR=0x70008000 -j $(grep -c processor /proc/cpuinfo) uImage modules imx53-usbarmory-gpio.dtb imx53-usbarmory-i2c.dtb imx53-usbarmory-spi.dtb imx53-usbarmory.dtb imx53-usbarmory-host.dtb imx53-usbarmory-scc2.dtb
 make modules_install INSTALL_MOD_PATH=${work_dir}
 cp arch/arm/boot/zImage ${work_dir}/boot/
 cp arch/arm/boot/dts/imx53-usbarmory*.dtb ${work_dir}/boot/
 make mrproper
 # Since these aren't integrated into the kernel yet, mrproper removes them
-cp ../usbarmory_linux-5.15.defconfig arch/arm/configs/
+cp ../usbarmory_linux-5.15_defconfig arch/arm/configs/
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-gpio.dts -O arch/arm/boot/dts/imx53-usbarmory-gpio.dts
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-host.dts -O arch/arm/boot/dts/imx53-usbarmory-host.dts
 wget $githubraw/f-secure-foundry/usbarmory/master/software/kernel_conf/mark-one/imx53-usbarmory-i2c.dts -O arch/arm/boot/dts/imx53-usbarmory-i2c.dts
@@ -170,10 +170,8 @@ sync
 
 status "u-Boot"
 cd "${base_dir}"
-wget ftp://ftp.denx.de/pub/u-boot/u-boot-2021.07.tar.bz2
-tar xvf u-boot-2021.07.tar.bz2 && cd u-boot-2021.07
-wget $githubraw/f-secure-foundry/usbarmory/master/software/u-boot/0001-Fix-microSD-detection-for-USB-armory-Mk-I.patch
-patch -p1 --no-backup-if-mismatch < 0001-Fix-microSD-detection-for-USB-armory-Mk-I.patch
+wget ftp://ftp.denx.de/pub/u-boot/u-boot-2022.01.tar.bz2
+tar xvf u-boot-2022.01.tar.bz2 && cd u-boot-2022.01
 make distclean
 make usbarmory_config
 make ARCH=arm
