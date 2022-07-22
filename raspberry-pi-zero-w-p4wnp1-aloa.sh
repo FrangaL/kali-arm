fb51f5a0a62bc16e9ccaeabb7019771fe87cc765
Steev Klimaszewski <steev@kali.org>
2022-07-22 16:39:31 -0500

    p4wnp1: Add ext4 mount command

--- raspberry-pi-zero-w-p4wnp1-aloa.sh
+++ raspberry-pi-zero-w-p4wnp1-aloa.sh
@@ -249,7 +249,11 @@ include rpi_firmware
 # Create the dirs for the partitions and mount them
 status "Create the dirs for the partitions and mount them"
 mkdir -p "${base_dir}"/root/
+if [[ $fstype == ext4 ]]; then
+mount -t ext4 -o noatime,data=writeback,barrier=0 "${rootp}" "${base_dir}"/root
+else
 mount "${rootp}" "${base_dir}"/root
+fi
 mkdir -p "${base_dir}"/root/boot
 mount "${bootp}" "${base_dir}"/root/boot
 
