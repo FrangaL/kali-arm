f4e97fd5d65e25a62f1e527163daa3c1b938f19d
Steev Klimaszewski <steev@kali.org>
2022-07-22 16:38:06 -0500

    remove test only comment and nobh has gone away

--- gateworks-ventana.sh
+++ gateworks-ventana.sh
@@ -133,9 +133,8 @@ make_fstab
 # Create the dirs for the partitions and mount them
 status "Create the dirs for the partitions and mount them"
 mkdir -p "${base_dir}"/root/
-# Test only
 if [[ $fstype == ext4 ]]; then
-mount -t ext4 -o noatime,data=writeback,barrier=0,nobh "${rootp}" "${base_dir}"/root
+mount -t ext4 -o noatime,data=writeback,barrier=0 "${rootp}" "${base_dir}"/root
 else
 mount "${rootp}" "${base_dir}"/root
 fi
