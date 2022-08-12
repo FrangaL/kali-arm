88775fea67a990f4337481e24361b66b6d32ad47
Ben Wilson <g0tmi1k@kali.org>
2022-08-12 13:20:07 +0100

    Refresh post-release script

--- bin/post-release.py
+++ bin/post-release.py
@@ -9,32 +9,32 @@
 ## This should be run after images are created.
 ##
 ## It parses the YAML sections of the devices.yml and creates:
-##
-## - "<outputdir>/rpi-imager.json": manifest file mapping image name to display name
+## - "<imagedir>/rpi-imager.json = "manifest file mapping image name to display name
 ##
 ## Dependencies:
-##   sudo apt -y install python3 python3-yaml python3-jinja2
+##   sudo apt -y install python3 python3-yaml xz-utils
 ##
 ## Usage:
-##  ./bin/post-release.py -i <input file> -o <output directory> -r <release>
+##  ./bin/post-release.py -i <input file> -r <release> -o <image directory>
 ##
 ## E.g.:
-## ./bin/post-release.py -i devices.yml -o images/ -r 2022.3
+## ./bin/post-release.py -i devices.yml -r 2022.3 -o images/
 
-import json
 import datetime
+import json
+import re
+import subprocess
 import yaml # python3 -m pip install pyyaml --user
 import getopt, os, stat, sys
-from jinja2 import Environment, FileSystemLoader
 
-manifest = ""     # Generated automatically (<outputdir>/rpi-imager.json)
+manifest = ""     # Generated automatically (<imagedir>/rpi-imager.json)
 release = ""
-outputdir = ""
+imagedir = ""
 inputfile = ""
-qty_images = 0
 qty_devices = 0
-environment = Environment(loader=FileSystemLoader("bin/"))
-template = environment.get_template("rpi-imager.j2")
+qty_images = 0
+qty_release_images = 0
+file_ext = ['xz', 'xz.sha256sum', 'sha256sum']
 
 ## Input:
 ## ------------------------------------------------------------ ##
@@ -44,14 +44,13 @@ template = environment.get_template("rpi-imager.j2")
 ## See: ./images/*.img.sha256sum (uncompressed image sha256sum - to get the sha256sum
 ##      ./images/*.img.xz.sha256sum (compressed image sha256sum - to get the sha256sum
 ##      ./images/*.img.xz (compressed image; we use xz to look at the metadata to get compressed/uncompressed size)
-##      unxz --verbose --list kali-linux-2022.2-pinebook-pro-arm64.img.xz | grep Compressed | awk '{ print substr($5,2); }' ## 401013776
-##      unxz --verbose --list kali-linux-2022.2-pinebook-pro-arm64.img.xz | grep Uncompressed | awk '{ print substr($5,2); }' ## 2254857830 
 
 def bail(message = "", strerror = ""):
     outstr = ""
     prog = sys.argv[0]
     if message != "":
         outstr = "\nError: {}".format(message)
+
     if strerror != "":
         outstr += "\nMessage: {}\n".format(strerror)
     else:
@@ -61,10 +60,10 @@ def bail(message = "", strerror = ""):
     sys.exit(2)
 
 def getargs(argv):
-    global inputfile, outputdir, release
+    global inputfile, imagedir, release
 
     try:
-        opts, args = getopt.getopt(argv,"hi:o:r:",["inputfile=","outputdir=","release="])
+        opts, args = getopt.getopt(argv,"hi:o:r:",["inputfile=","imagedir=","release="])
     except getopt.GetoptError as e:
         bail("Incorrect arguments: {}".format(e))
 
@@ -74,10 +73,10 @@ def getargs(argv):
                 bail()
             elif opt in ("-i", "--inputfile"):
                 inputfile = arg
-            elif opt in ("-o", "--outputdir"):
-                outputdir = arg.rstrip("/")
             elif opt in ("-r", "--release"):
                 release = arg
+            elif opt in ("-o", "--imagedirectory"):
+                imagedir = arg.rstrip("/")
             else:
                 bail("Unrecognised argument: " + opt)
     else:
@@ -95,15 +94,25 @@ def yaml_parse(content):
             result += line + "\n"
     return yaml.safe_load(result)
 
-def jsonarray(devices, vendor, name, filename):
+def jsonarray(devices, vendor, name, url, extract_size, extract_sha256, image_download_size, image_download_sha256):
     if not vendor in devices:
         devices[vendor] = []
-    jsondata = {"name": name, "filename": filename}
+
+    jsondata = {"name": name,
+                "description": "Kali Linux ARM image for the {}".format(name),
+                "url": url,
+                "icon": "https://www.kali.org/images/favicon.png",
+                "release_date": datetime.datetime.today().strftime("%Y-%m-%d"),
+                "extract_size": extract_size,
+                "extract_sha256": extract_sha256,
+                "image_download_size": "{}".format(image_download_size),
+                "image_download_sha256": image_download_sha256,
+                "website": "https://www.kali.org/"}
     devices[vendor].append(jsondata)
     return devices
 
 def generate_manifest(data):
-    global release, qty_devices, qty_images
+    global release, qty_devices, qty_images, qty_release_images
     default = ""
     devices = {}
 
@@ -111,6 +120,11 @@ def generate_manifest(data):
     for yaml in data['devices']:
         # Iterate over vendors
         for vendor in yaml.keys():
+            # @g0tmi1k: Feels like there is a cleaner way todo this
+            if not vendor == "raspberrypi":
+                continue
+            # Ready to have a unique name in the entry
+            img_seen = set()
             # Iterate over board (depth 2)
             for board in yaml[vendor]:
                 qty_devices += 1
@@ -118,25 +132,44 @@ def generate_manifest(data):
                 for key in board.keys():
                     # Check if there is an image for the board
                     if 'images' in key:
-                        # Check that it's not eol or community supported (depth 3)
-                        # Iterate over image (depth 4)
+                        # Iterate over image (depth 3)
                         for image in board[key]:
-                            if image.get('image').startswith("raspberry"):
-                                qty_images += 1
+                            qty_images += 1
+                            # Check that it's not EOL or community supported
+                            if image.get('support') == "kali":
                                 name = image.get('name', default)
-                                filename = "kali-linux-{}-{}".format(release, image.get('image', default))
-                                jsonarray(devices, vendor, name, filename)
-    return json.dumps(devices, indent = 2)
+                                # If we haven't seen this image before for this vendor
+                                if name not in img_seen:
+                                    img_seen.add(name)
+                                    qty_release_images += 1
 
-def deduplicate(data):
-    # Remove duplicate lines
-    clean_data = ""
-    lines_seen = set()
-    for line in data.splitlines():
-        if line not in lines_seen: # not a duplicate
-            clean_data += line + "\n"
-            lines_seen.add(line)
-    return clean_data
+                                    filename = "kali-linux-{}-{}".format(release, image.get('image', default))
+
+                                    # Check to make sure files got created
+                                    for ext in file_ext:
+                                        check_file = '{}/{}.{}'.format(imagedir, filename, ext)
+                                        if not os.path.isfile(check_file):
+                                            bail("Missing: '{}'".format(check_file), "Please create the image before running")
+
+                                    with open('{}/{}.xz.sha256sum'.format(imagedir, filename)) as f:
+                                        image_download_sha256 = f.read().split()[0]
+                                    with open('{}/{}.sha256sum'.format(imagedir, filename)) as f:
+                                        extract_sha256 = f.read().split()[0]
+
+                                    url = "https://kali.download/arm-images/kali-{}/{}.xz".format(release, filename)
+
+                                    # @g0tmi1k: not happy about external OS, rather keep it in python (import lzma)
+                                    try:
+                                        unxz = subprocess.check_output("unxz --verbose --list {}/{}.xz | grep 'Uncompressed'".format(imagedir, filename), shell=True)
+                                        extract_size = re.findall(r'\((.*?) B\)', str(unxz))[0]
+                                    except subprocess.CalledProcessError as e:
+                                        #print("command '{}' return with error (code {})".format(e.cmd, e.returncode))
+                                        extract_size = "0"
+
+                                   #image_download_size = os.stat('{}/{}.xz'.format(imagedir, filename)).st_size
+                                    image_download_size = os.path.getsize('{}/{}.xz'.format(imagedir, filename))
+                                    jsonarray(devices, 'os_list', name, url, extract_size, extract_sha256, image_download_size, image_download_sha256)
+    return json.dumps(devices, indent = 2)
 
 def createdir(dir):
     try:
@@ -165,7 +198,7 @@ def writefile(data, file):
     return 0
 
 def main(argv):
-    global inputfile, outputdir, release
+    global inputfile, imagedir, release
 
     # Parse command-line arguments
     if len(sys.argv) > 1:
@@ -174,24 +207,24 @@ def main(argv):
         bail("Missing arguments")
 
     # Assign variables
-    manifest = outputdir + "/rpi-imager.json"
+    manifest = imagedir + "/rpi-imager.json"
     data = readfile(inputfile)
 
     # Get data
     res = yaml_parse(data)
-    #manifest_list = deduplicate(generate_manifest(res))
     manifest_list = generate_manifest(res)
 
     # Create output directory if required
-    createdir(outputdir)
+    createdir(imagedir)
 
     # Create manifest file
     writefile(manifest_list, manifest)
 
     # Print result and exit
     print('\nStats:')
-    print('  - Devices\t: {}'.format(qty_devices))
-    print('  - Images\t: {}'.format(qty_images))
+    print('  - Total devices\t: {}'.format(qty_devices))
+    print('  - Total images\t: {}'.format(qty_images))
+    print('  - {} rpi images\t: {}'.format(release, qty_release_images))
     print("\n")
     print('Manifest file created\t: {}'.format(manifest))
 
