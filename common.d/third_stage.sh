351f953ef06b42d272009e72811f8c70ce0ed65a
Steev Klimaszewski <steev@kali.org>
2022-08-16 14:06:58 -0500

    common: 3stage: Don't need to clean up eatmydata

--- common.d/third_stage.sh
+++ common.d/third_stage.sh
@@ -4,10 +4,6 @@
 cat <<EOF >> "${work_dir}"/third-stage
 status_stage3 'Clean up apt-get'
 eatmydata apt-get -y --purge autoremove
-
-status_stage3 'Clean up dpkg.eatmydata'
-rm -f /usr/bin/dpkg
-dpkg-divert --remove --rename /usr/bin/dpkg
 EOF
 
 # Run third stage
