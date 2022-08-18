478ebb3a1c69e17c2b10ac8731676bdb31174a95
Arszilla <arszilla@arszilla.com>
2022-08-18 23:20:26 +0300

    Improve generate_devices_stats.py

--- bin/generate_devices_stats.py
+++ bin/generate_devices_stats.py
@@ -2,31 +2,41 @@
 import sys
 from datetime import datetime
 
-import yaml # python3 -m pip install pyyaml --user
+import yaml  # python3 -m pip install pyyaml --user
 
 OUTPUT_FILE = './device-stats.md'
 INPUT_FILE = './devices.yml'
-repo_msg = "\n_This table was [generated automatically](https://gitlab.com/kalilinux/build-scripts/kali-arm/-/blob/master/devices.yml) on {} from the [Kali ARM GitLab repository](https://gitlab.com/kalilinux/build-scripts/kali-arm)_\n".format(datetime.now().strftime("%Y-%B-%d %H:%M:%S"))
+
+repo_msg = "\n_This table was [generated automatically](https://gitlab.com/kalilinux/build-scripts/kali-arm/-/blob/master/devices.yml) on {} from the [Kali ARM GitLab repository](https://gitlab.com/kalilinux/build-scripts/kali-arm)_\n".format(
+    datetime.now().strftime("%Y-%B-%d %H:%M:%S"))
+
 qty_devices = 0
 qty_images = 0
 
-## Input:
-## ------------------------------------------------------------ ##
-## See: ./devices.yml
-##      https://gitlab.com/kalilinux/build-scripts/kali-arm/-/blob/master/devices.yml
+# Input:
+# ------------------------------------------------------------
+# See: ./devices.yml
+# https://gitlab.com/kalilinux/build-scripts/kali-arm/-/blob/master/devices.yml
+
 
 def yaml_parse(content):
     result = ""
+
     lines = content.split('\n')
+
     for line in lines:
         if line.strip() and not line.strip().startswith('#'):
             result += line + "\n"
+
     return yaml.safe_load(result)
+
 
 def generate_table(data):
     global qty_devices, qty_images
+
     default = ""
-    table  = "| Vendor | [Board](devices.html) | [Images](images.html) |\n"
+
+    table = "| Vendor | [Board](devices.html) | [Images](images.html) |\n"
     table += "|--------|-----------------------|-----------------------|\n"
 
     # Iterate over per input (depth 1)
@@ -37,41 +47,56 @@ def generate_table(data):
             for board in yaml[vendor]:
                 qty_devices += 1
                 qty_images += len(board.get('images', default))
+
                 table += "| {} | {} | {} |\n".format(vendor,
-                                                     board.get('name', default),
+                                                     board.get(
+                                                         'name', default),
                                                      len(board.get('images', default)))
+
     return table
+
 
 def read_file(file):
     try:
         with open(file) as f:
             data = f.read()
             f.close()
+
     except Exception as e:
         print("[-] Cannot open input file: {} - {}".format(file, e))
+
     return data
+
 
 def write_file(data, file):
     try:
         with open(file, 'w') as f:
-            meta  = '---\n'
+            meta = '---\n'
             meta += 'title: Kali ARM Device Statistics\n'
             meta += '---\n\n'
-            stats  = "- The official [Kali ARM repository](https://gitlab.com/kalilinux/build-scripts/kali-arm) contains [build-scripts]((https://gitlab.com/kalilinux/build-scripts/kali-arm)) to support [**{}** Kali ARM devices](devices.html)\n".format(str(str(qty_devices)))
+
+            stats = "- The official [Kali ARM repository](https://gitlab.com/kalilinux/build-scripts/kali-arm) contains [build-scripts]((https://gitlab.com/kalilinux/build-scripts/kali-arm)) to support [**{}** Kali ARM devices](devices.html)\n".format(
+                str(str(qty_devices)))
             stats += "- [Kali ARM Statistics](index.html)\n\n"
+
             f.write(str(meta))
             f.write(str(stats))
             f.write(str(data))
             f.write(str(repo_msg))
             f.close()
+
             print('[+] File: {} successfully written'.format(OUTPUT_FILE))
+
     except Exception as e:
         print("[-] Cannot write to output file: {} - {}".format(file, e))
+
     return 0
+
 
 def print_summary():
     print('Devices: {}'.format(qty_devices))
     print('Images : {}'.format(qty_images))
+
 
 def main(argv):
     # Assign variables
@@ -89,6 +114,7 @@ def main(argv):
 
     # Exit
     exit(0)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
