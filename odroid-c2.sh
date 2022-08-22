371c1f90dc2751dd60cf3d66a1a0032f6b76f095
Arszilla <arszilla@arszilla.com>
2022-08-22 22:59:54 +0300

    Update and fix odroid-c2.sh

--- odroid-c2.sh
+++ odroid-c2.sh
@@ -9,8 +9,10 @@
 
 # Hardware model
 hw_model=${hw_model:-"odroid-c2"}
+
 # Architecture
 architecture=${architecture:-"arm64"}
+
 # Desktop manager (xfce, gnome, i3, kde, lxde, mate, e17 or none)
 desktop=${desktop:-"xfce"}
 
@@ -22,7 +24,7 @@ basic_network
 add_interface eth0
 
 # Third stage
-cat <<EOF >> "${work_dir}"/third-stage
+cat <<EOF >>"${work_dir}"/third-stage
 status_stage3 'Copy odroid-c2 services'
 cp -p /bsp/services/odroid-c2/*.service /etc/systemd/system/
 
@@ -31,7 +33,7 @@ status_stage3 'Using the fbdev driver is not ideal but it is far less frustratin
 mkdir -p /etc/X11/xorg.conf.d
 cp -p /bsp/xorg/20-meson.conf /etc/X11/xorg.conf.d/
 
-status_stage3 'Install the kernel packages'
+status_stage3 'Install the kernel packages'>
 eatmydata apt-get install -y dkms linux-image-arm64 u-boot-menu
 
 # We will replace this later, via sed, to point to the correct root partition (hopefully?)
@@ -53,7 +55,7 @@ include clean_system
 # This requires 2 files, a script and then something for lightdm to use
 # I do not have anything set up for the console though, so that's still broken for now
 mkdir -p ${work_dir}/usr/local/bin
-cat << EOF > ${work_dir}/usr/local/bin/xrandrscript.sh
+cat <<EOF >${work_dir}/usr/local/bin/xrandrscript.sh
 #!/usr/bin/env bash
 
 resolution=$(xdpyinfo | awk '/dimensions:/ { print $2; exit }')
@@ -62,12 +64,15 @@ if [[ "$resolution" == "1366x768" ]]; then
     xrandr --newmode "1360x768_60.00"   84.75  1360 1432 1568 1776  768 771 781 798 -hsync +vsync
     xrandr --addmode HDMI-1 1360x768_60.00
     xrandr --output HDMI-1 --mode  1360x768_60.00
+
 fi
 EOF
+
 chmod 0755 ${work_dir}/usr/local/bin/xrandrscript.sh
 
 mkdir -p ${work_dir}/usr/share/lightdm/lightdm.conf.d/
-cat << EOF > ${work_dir}/usr/share/lightdm/lightdm.conf.d/60-xrandrscript.conf
+
+cat <<EOF >${work_dir}/usr/share/lightdm/lightdm.conf.d/60-xrandrscript.conf
 [SeatDefaults]
 display-setup-script=/usr/local/bin/xrandrscript.sh
 session-setup-script=/usr/local/bin/xrandrscript.sh
@@ -85,8 +90,10 @@ parted -s -a minimal "${image_dir}/${image_name}.img" mkpart primary $fstype 32M
 
 # Set the partition variables
 make_loop
+
 # Create file systems
 mkfs_partitions
+
 # Make fstab.
 make_fstab
 
@@ -96,15 +103,17 @@ mkdir -p "${base_dir}"/root/
 mount "${rootp}" "${base_dir}"/root
 
 status "Edit the extlinux.conf file to set root uuid and proper name"
+
 # Ensure we don't have root=/dev/sda3 in the extlinux.conf which comes from running u-boot-menu in a cross chroot
 # We do this down here because we don't know the UUID until after the image is created
 sed -i -e "0,/root=.*/s//root=UUID=$root_uuid rootfstype=$fstype console=tty1 consoleblank=0 ro rootwait/g" ${work_dir}/boot/extlinux/extlinux.conf
+
 # And we remove the "GNU/Linux because we don't use it
 sed -i -e "s|.*GNU/Linux Rolling|menu label Kali Linux|g" ${work_dir}/boot/extlinux/extlinux.conf
 
 status "Set the default options in /etc/default/u-boot"
-echo 'U_BOOT_MENU_LABEL="Kali Linux"' >> ${work_dir}/etc/default/u-boot
-echo 'U_BOOT_PARAMETERS="console=tty1 consoleblank=0 ro rootwait"' >> ${work_dir}/etc/default/u-boot
+echo 'U_BOOT_MENU_LABEL="Kali Linux"' >>${work_dir}/etc/default/u-boot
+echo 'U_BOOT_PARAMETERS="console=tty1 consoleblank=0 ro rootwait"' >>${work_dir}/etc/default/u-boot
 
 status "Rsyncing rootfs into image file"
 rsync -HPavz -q "${work_dir}"/ "${base_dir}"/root/
@@ -130,6 +139,7 @@ git clone --depth 1 https://github.com/hardkernel/u-boot -b odroidc2-v2015.01 u-
 # First things first, let's build the meson-tools, of which, we only really need amlbootsig
 cd ${base_dir}/bootloader/meson-tools/
 make
+
 # Now we need to build fip_create
 cd ${base_dir}/bootloader/u-boot-hk/tools/fip_create
 HOSTCC=cc HOSTLD=ld make
@@ -148,17 +158,21 @@ make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu-
 
 # This is funky, but in the end, it should do the right thing
 cd ${base_dir}/bootloader/
+
 # Create the fip.bin
 ./u-boot-hk/tools/fip_create/fip_create --bl30 ./u-boot-hk/fip/gxb/bl30.bin \
---bl301 ./u-boot-hk/fip/gxb/bl301.bin --bl31 ./u-boot-hk/fip/gxb/bl31.bin \
---bl33 u-boot/u-boot.bin fip.bin
+    --bl301 ./u-boot-hk/fip/gxb/bl301.bin --bl31 ./u-boot-hk/fip/gxb/bl31.bin \
+    --bl33 u-boot/u-boot.bin fip.bin
 
 # Create the stage2 bootloader thingie?
-cat ./u-boot-hk/fip/gxb/bl2.package fip.bin > boot_new.bin
+cat ./u-boot-hk/fip/gxb/bl2.package fip.bin >boot_new.bin
+
 # Now sign it, and call it u-boot.bin
 ./meson-tools/amlbootsig boot_new.bin u-boot.bin
+
 # Now strip a portion of it off, and put it in the sd_fuse directory
 dd if=u-boot.bin of=./u-boot-hk/sd_fuse/u-boot.bin bs=512 skip=96
+
 # Finally, write it to the loopdevice so we have our bootloader on the card
 cd ./u-boot-hk/sd_fuse
 ./sd_fusing.sh ${loopdevice}
